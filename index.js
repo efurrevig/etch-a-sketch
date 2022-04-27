48bf8fc89ab5750e7284cddfd5deab4d038c8935
Eric Furrevig <efurrevig@gmail.com>
2022-04-26 20:25:59 -0400

    Fixed grid expanding past container

--- index.js
+++ index.js
@@ -10,36 +10,36 @@ function newBox() {
     if (foo === null) {
         return;
     }
-    while (isNaN(foo) || foo > 100) {
+    while (isNaN(foo) || foo > 64) {
         foo = prompt("Please insert a number less than 100");
     };
     clearBox("container")
-    makeGrid(food);
+    makeGrid(foo);
 }
 
 function makeGrid(size) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows*cols); c++) {
+    container.style.setProperty('--grid-rows', size);
+    container.style.setProperty('--grid-cols', size);
+    for (c = 0; c < (size*size); c++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
         cell.addEventListener('mouseover', e => e.target.classList.add('hover-class'));
     };
 };
 
-grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+/*grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
- /* for (let i = 0; i < size * size; i++) {
+ for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div')
     gridElement.classList.add("grid-element")
     gridElement.addEventListener('mouseover', changeColor)
     gridElement.addEventListener('mousedown', changeColor)
     c.appendChild(gridElement)
   }
+
+
 */
 
-
-
-makeGrid(16, 16);
+makeGrid(16);
 
